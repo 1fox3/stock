aa77f230dbac625a5eb11c011c0de82cb6d989c1
lusongsong2 <lusongsong@jd.com>
2020-05-02 10:38:55 +0800

    计划任务管理

---
+++ js/quartz/quartz.js
@@ -0,0 +1,15 @@
+var quartzApiHost = "http://api.1fox3.com/admin/quartz";
+var quartzJobListUrl = quartzApiHost + "/job/getListByGroup";
+var quartzJobInfoUrl = quartzApiHost + "/job/info";
+var quartzJobCreateUrl = quartzApiHost + "/job/create";
+var quartzJobUpdateUrl = quartzApiHost + "/job/update";
+var quartzJobDeleteUrl = quartzApiHost + "/job/delete";
+var quartzJobStartUrl = quartzApiHost + "/jobManage/start";
+var quartzJobPauseUrl = quartzApiHost + "/jobManage/pause";
+var quartzJobResumeUrl = quartzApiHost + "/jobManage/resume";
+var quartzJobStopUrl = quartzApiHost + "/jobManage/stop";
+var quartzJobParamListUrl = quartzApiHost + "/jobParam/getByJob";
+var quartzJobParamInfoUrl = quartzApiHost + "/jobParam/info";
+var quartzJobParamCreateUrl = quartzApiHost + "/jobParam/create";
+var quartzJobParamUpdateUrl = quartzApiHost + "/jobParam/update";
+var quartzJobParamDeleteUrl = quartzApiHost + "/jobParam/delete";
